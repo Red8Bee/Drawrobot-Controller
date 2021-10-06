6eb637e03539e145a389dea0dfa4f84b01d771c4
Red8Bee <44880816+Red8Bee@users.noreply.github.com>
2021-10-06 14:27:05 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,44 @@
 input.onButtonPressed(Button.A, function () {
     basic.showLeds(`
-        . . . . .
-        . # . # .
         . . # . .
-        . # . # .
-        . . . . .
-        `)
-    radio.sendString("right")
-})
-input.onButtonPressed(Button.AB, function () {
-    basic.showLeds(`
-        . . . . .
+        . # . . .
+        # # # # #
         . # . . .
         . . # . .
-        . . . # .
-        . . . . .
         `)
+    radio.sendString("left")
+})
+input.onButtonPressed(Button.AB, function () {
+    if (stopped) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        stopped = 0
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            `)
+        stopped += 1
+    }
     radio.sendString("stop")
 })
 input.onButtonPressed(Button.B, function () {
     basic.showLeds(`
-        . . . . .
+        . . # . .
+        . . . # .
+        # # # # #
         . . . # .
         . . # . .
-        . # . . .
-        . . . . .
         `)
-    radio.sendString("left")
+    radio.sendString("right")
 })
+let stopped = 0
 radio.setGroup(1)
